dc93ca460d3a16ff662a2f53b2d83cffa9a16a8d
thelazyone <giacomo.pantalone@gmail.com>
2023-03-16 22:05:52 +0000

    refactoring: new grid collision and .js prettiness boost

--- www/index.old.js
+++ www/index.old.js
@@ -1,21 +1,20 @@
 import { Petri, Bacter } from "bacter_rust";
+
+// the _bg calls for the wasm linear memory
 import { memory } from "bacter_rust/bacter_rust_bg";
 
-// Creating the petri dish. The amount of bacters is 100 for a 500x500 dish
-const pre = document.getElementById("bacter_canvas");
-pre.width = window.innerWidth;
-pre.height = window.innerHeight;const total_bacters = 500 / (500*500) * pre.width * pre.height;
-const universe = Petri.new_with_params(pre.width, pre.height, total_bacters);
-console.warn(pre.width, pre.height, 100);
-
+const pre = document.getElementById("stats_canvas"); 
+const universe = Petri.new();
 var averageTickTime = 0.
-var fastMode = false;
 
-// Attaching the button
-document.getElementById("fast_btn").onclick = toggleFast;
+// Preparing the chart:
+google.charts.load('current', {
+  'packages':['corechart']
+});
+google.charts.setOnLoadCallback(drawChart);
 
 // Population Chart variables
-var population_data = [['Iteration', 'Bacters', 'Algae']];
+var population_data = [['Iteration', 'Bacters', 'Algae']]; 
 var population_options = {
       title: 'Global Populations',
       curveType: 'none',
@@ -31,13 +30,14 @@ var performances_options = {
 };
 
 // Parameters Distribution Chart variables
-var params_distribution_data = [['Aggressivity', 'Size']];
+var params_distribution_data = [['Aggressivity', 'Size']]; 
 var params_distribution_options = {
       title: 'Aggressivity vs. Size projection',
       hAxis: {title: 'Aggressivity', minValue: 0., maxValue: 1.},
       vAxis: {title: 'Size', minValue: 0.2, maxValue: 1.},
       legend: 'none'
     };
+
 
 function drawChart() {
   let bacters_number = universe.get_bacters_number();
@@ -55,22 +55,33 @@ function drawChart() {
   for (let i = 0; i < bacters_number; i++) {
     params_distribution_data.push([aggros[i] , sizes[i]]);
   }
+
+  // Drawing the populations plot:
+  var population_chart = new google.visualization.LineChart(document.getElementById('data_chart'));
+  population_chart.draw(google.visualization.arrayToDataTable(population_data), population_options);
+
+  // Drawing the performances plot:
+  var performances_chart = new google.visualization.LineChart(document.getElementById('performances_chart'));
+  performances_chart.draw(google.visualization.arrayToDataTable(performances_data), performances_options);
+
+  // Drawing the last-iteration parameters space:
+  var params_distribution_chart = new google.visualization.ScatterChart(document.getElementById('params_chart'));
+  params_distribution_chart.draw(google.visualization.arrayToDataTable(params_distribution_data), params_distribution_options);
 }
 
 // Generic looping render.
 const renderLoop = () => {
 
-  // Now, depending whether the "SLOW" button is pressed, the
+  // Now, depending wether the "SLOW" button is pressed, the 
   // simulation proceeds by one tick or by 1000.
 
+  if(document.getElementById('slow_button').checked == true)
+  {
     // Ticking by one iteration:
-    if (fastMode){
-        universe.tick(1000);
-    }
-    else
-    {
-        universe.tick(10);
-    }
+    universe.tick(1);
+
+    // Writing statistics as string
+    pre.textContent = universe.get_stats_string();
 
     // Reading the linear memory for size and aggro:
     let bacters_number = universe.get_bacters_number();
@@ -85,36 +96,46 @@ const renderLoop = () => {
     const algae = new Float32Array(memory.buffer, algae_ptr, algae_number * 2);
 
     // Filling the canvas:
-    var canvas = document.getElementById('bacter_canvas');
+    var canvas = document.getElementById('petri_canvas');
     let ctx = canvas.getContext('2d')
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.rect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#1B222C";
-    ctx.fill();
     for (let i = 0; i < bacters_number; i++) {
-      drawCircle(ctx, positions[2*i], positions[2*i + 1], sizes[i] * 10., 'white', '', 2)
+      drawCircle(ctx, positions[2*i], positions[2*i + 1], sizes[i] * 10., 'black', '', 2)
     }
     for (let i = 0; i < algae_number; i++) {
-      drawCircle(ctx, algae[2*i], algae[2*i + 1], 1., '#AFFC8C', '', 2)
+      drawCircle(ctx, algae[2*i], algae[2*i + 1], 1., 'green', '', 2)
     }
+  }
+  else
+  {
+    // Ticking the universe for a thousands iterations: 
+    var startTime = new Date();
+    universe.tick(1000);
+    var endTime = new Date();
+    var diffTime = endTime - startTime;
+    averageTickTime = averageTickTime * 0.9 + diffTime;
 
-    // Updating basic stats.
-    document.getElementById("stats").textContent="Total Bacters: " + bacters_number + " size " + pre.width + " " + pre.height;
-    document.getElementById("iteration").textContent="iteration #" + universe.get_iteration();
+    // Writing statistics as string
+    var tmpString = (averageTickTime / 10).toString()
+    pre.textContent = universe.get_stats_string() + " avg elapsed: " + tmpString + " ms.";
 
-  // looping, as always, with a time frame set
-    if (fastMode) {
-        requestAnimationFrame(renderLoop);
-    }
-    else {
-        setTimeout(() => {
-            requestAnimationFrame(renderLoop);
-        }, 1000 / 60);
-    }
+    // the populations growth plot needs an update at every tick (unregarding to the refresh rate)
+    population_data.push([universe.get_iteration(),universe.get_bacters_number(), universe.get_algae_number()]); 
+
+    performances_data.push([universe.get_iteration(), averageTickTime / 10]); 
+    // console.log(population_data.length);
+  }
+
+  // looping, as always.
+  requestAnimationFrame(renderLoop);
 };
+
+// Refreshing the image only when necessary! 
+var intervalId = setInterval(function() { google.charts.setOnLoadCallback(drawChart); }, 500);
 
 // Calling this once, it enters into an infinite loop.
 requestAnimationFrame(renderLoop)
+
 
 // CANVAS CIRCLES DRAWING:
 // Taken from https://stackoverflow.com/questions/25095548/how-to-draw-a-circle-in-html5-canvas-using-javascript
@@ -130,9 +151,4 @@ function drawCircle(ctx, x, y, radius, fill, stroke, strokeWidth) {
     ctx.strokeStyle = stroke
     ctx.stroke()
   }
-}
-
-// Buttons Functions:
-function toggleFast() {
-    fastMode = !fastMode;
 }
